805c2417525bc29dbf82e987cb197e631227423b
Cesar Vasquez <heellxz@gmail.com>
2018-01-27 23:47:19 -0300

    puerto 8080

--- __main__.py
+++ __main__.py
@@ -4,7 +4,7 @@ from chinchilla import app
 if __name__ == '__main__':
 	HOST = environ.get('SERVER_HOST', 'localhost')
 	try:
-		PORT = int(environ.get('SERVER_PORT', '80'))
+		PORT = int(environ.get('SERVER_PORT', '8080'))
 	except ValueError:
-		PORT = 80
+		PORT = 8080
 	app.run(HOST, PORT, True, threaded=True)
